e1745588e7b26b8d82df62f3adfd4993883b752e
Andrew <nevrmdfr@gmail.com>
2023-11-01 18:19:13 +0300

    Logo and Start button

--- app/src/main/java/com/nevrmd/sevenminworkout/MainActivity.kt
+++ app/src/main/java/com/nevrmd/sevenminworkout/MainActivity.kt
@@ -2,10 +2,20 @@ package com.nevrmd.sevenminworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.nevrmd.sevenminworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.llStart.setOnClickListener {
+            Toast.makeText(this, "Works!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
